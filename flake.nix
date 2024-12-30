1a0ce252e451693fd821a0a3ac810f817e3cd54c
0x5a4 <54070204+0x5a4@users.noreply.github.com>
2024-12-30 22:54:27 +0100

    nixify a bit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            openjdk8
+            (gradle_7.override {
+              java = pkgs.openjdk8;
+            })
+          ];
+
+          JAVA_HOME = pkgs.openjdk8.home;
+        };
+      }
+    );
+}
